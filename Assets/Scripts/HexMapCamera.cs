17a23ec32d2156da07220fad4236c2c30bd662cb
DESKTOP-AAFLFEM\Enis <enis.isik@ogr.iu.edu.tr>
2023-08-14 13:55:52 +0300

    added rotation to camera movement code

--- Assets/Scripts/HexMapCamera.cs
+++ Assets/Scripts/HexMapCamera.cs
@@ -9,6 +9,7 @@ public class HexMapCamera : MonoBehaviour
 	public float stickMinZoom, stickMaxZoom;
 	public float swivelMinZoom, swivelMaxZoom;
 	public float moveSpeedMinZoom, moveSpeedMaxZoom;
+	public float rotationSpeed;
 
 	void Awake()
 	{
@@ -23,6 +24,12 @@ public class HexMapCamera : MonoBehaviour
 			AdjustZoom(zoomDelta);
 		}
 
+		float rotationDelta = Input.GetAxis("Rotation");
+		if (rotationDelta != 0f)
+		{
+			AdjustRotation(rotationDelta);
+		}
+
 		float xDelta = Input.GetAxis("Horizontal");
 		float zDelta = Input.GetAxis("Vertical");
 		if (xDelta != 0f || zDelta != 0f)
@@ -34,7 +41,7 @@ public class HexMapCamera : MonoBehaviour
 	public float moveSpeed;
 	void AdjustPosition(float xDelta, float zDelta)
 	{
-		Vector3 direction = new Vector3(xDelta, 0f, zDelta).normalized;
+		Vector3 direction = transform.localRotation * new Vector3(xDelta, 0f, zDelta).normalized;
 		float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
 		float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom)  * damping * Time.deltaTime;
 
@@ -54,4 +61,18 @@ public class HexMapCamera : MonoBehaviour
 		swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
 	}
 
+	float rotationAngle;
+	void AdjustRotation(float delta)
+	{
+		rotationAngle += delta * rotationSpeed * Time.deltaTime;
+		if (rotationAngle < 0f)
+		{
+			rotationAngle += 360f;
+		}
+		else if (rotationAngle >= 360f)
+		{
+			rotationAngle -= 360f;
+		}
+		transform.localRotation = Quaternion.Euler(0f, rotationAngle, 0f);
+	}
 }
